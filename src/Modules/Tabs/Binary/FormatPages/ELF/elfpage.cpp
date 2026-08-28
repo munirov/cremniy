c1153e88a94d5f04f6104c74a0f3ab9f5a44cd61
Alexsey <alexsey.websites.work@gmail.com>
2026-08-28 23:36:58 +0300

    Update table header labels from Russian to English

--- src/Modules/Tabs/Binary/FormatPages/ELF/elfpage.cpp
+++ src/Modules/Tabs/Binary/FormatPages/ELF/elfpage.cpp
@@ -28,7 +28,7 @@ ELFPage::ELFPage(QWidget *parent)
     layout->addWidget(m_warning);
 
     m_table = new QTableWidget(0, 2, this);
-    m_table->setHorizontalHeaderLabels({"Поле", "Значение"});
+    m_table->setHorizontalHeaderLabels({"Field", "Meaning"}); 
     m_table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     m_table->setEditTriggers(QAbstractItemView::NoEditTriggers);
     m_table->verticalHeader()->setVisible(false);
